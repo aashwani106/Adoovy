2fe87f78e85897252b9e1541fb00f9fdfabe5519
Ashwani Mishra <ashwani01superman@gmail.com>
2024-04-22 01:26:04 +0530

    backend router setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,3 +7,8 @@ dotenv.config({
 app.listen(process.env.PORT || 8000, () => {
   console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
 });
+
+
+// routes import
+import homeRouter from './routes/home/home.routes.js';
+app.use("/api/v1/home" , homeRouter)
