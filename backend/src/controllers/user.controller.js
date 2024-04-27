a85f5ddb5dd90d7be6830b68d2358096f82aa2a6
Ashwani Mishra <ashwani01superman@gmail.com>
2024-04-27 22:23:57 +0530

    db setup

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,11 +1,27 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User} from "../models/user.model.js"
 
 const registerUser = asyncHandler( async (req,res) => {
+
+  console.log('body' , req.body )
+  return
+  let fullName = 'ax'
+  let email = 'ex';
+  let username = 'dx'
+  let password ='dx'
+  const user = await User.create({
+    fullName,
+    email, 
+    password,
+    username: username.toLowerCase()
+})
 
   res.status(200).json({
     message : "okks"
   })
-  
+
+
+
 });
 
 export { registerUser };
--- backend/src/routes/home/home.routes.js
+++ backend/src/routes/home/home.routes.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 // import { registerUser } from "../../controllers/user.controller.js"
 const router = Router();
 
-router.get("/", (req, res) => {
+router.post("/", (req, res) => {
+  console.log(req.body)
   res.send("Hello World again..!");
 });
 // router.post('/register', registerUser )      method 1
